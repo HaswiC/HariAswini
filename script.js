e2108fa7837157259a427bb5059a57aa347135e2
HaswiC <mvgharic@gmail.com>
2025-02-10 12:51:15 +0530

    Update and rename js to script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+// Countdown Timer
+const weddingDate = new Date("2025-02-17T04:30:00").getTime();
+
+function updateCountdown() {
+  const now = new Date().getTime();
+  const timeLeft = weddingDate - now;
+
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+
+  if (timeLeft < 0) {
+    clearInterval(interval);
+    document.getElementById("timer").innerHTML = "The wedding has started!";
+  }
+}
+
+const interval = setInterval(updateCountdown, 1000);
+updateCountdown();
